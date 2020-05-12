f8c0e27c66107442761daf5a53e38ebf275ca82a
Stein Somers <git@steinsomers.be>
2020-05-13 00:04:16 +0200

    Timeout independent of interrupt; refinements

--- arduino_RF433_chacon.ino
+++ arduino_RF433_chacon.ino
@@ -1,20 +1,28 @@
 static const int PIN_DIGITAL_IN = 2;
-static const int MIN_PEAK_SPACING = 500;
-static const int MAX_PEAK_SPACING = 600;
-static const int MAX_VALE_SPACING = 1500;
-static const int MIN_PACKET_SPACING = 10000;
-static const int MIN_PACKET_PREAMBLE = 2800;
 static const bool VERBOSE = false;
+#define DIAGNOSTICS 0
+
+static const unsigned long MIN_PEAK_SPACING = 500;
+static const unsigned long MAX_PEAK_SPACING = 600;
+static const unsigned long MAX_VALE_SPACING = 1500;
+static const unsigned long MIN_PACKET_SPACING = 10000;
+static const unsigned long MAX_PACKET_SPACING = 11000;
+static const unsigned long MIN_PACKET_PREAMBLE = 2800;
+static const unsigned long MAX_PACKET_PREAMBLE = 3200;
+static const unsigned long MAX_PAYLOAD_TIMEOUT = 32ul * (MAX_VALE_SPACING + 3 * MAX_PEAK_SPACING);
 
 class ProtocolHandler {
-  unsigned long last_time = micros();
+  unsigned long last_rise_micros = micros();
   unsigned long packet_handled = 0;
-  enum { IDLE = -2, DELIMITED = -1, OPENED = 0 };
+  unsigned long packet_timeout_micros;
+  enum { IDLE = -3, BOTCHED_PACKET = -2, DELIMITED = -1, OPENED = 0 };
   int reception_stage;
   int extra_peaks_received;
+  unsigned long bits_received;
+#if DIAGNOSTICS
   unsigned long delimiter_micros;
   unsigned long preamble_micros;
-  unsigned long bits_received;
+#endif
 
   void abort_packet_train() {
     digitalWrite(LED_BUILTIN, LOW);
@@ -23,7 +31,7 @@ class ProtocolHandler {
   }
 
   void cancel_packet() {
-    reception_stage = IDLE;
+    reception_stage = BOTCHED_PACKET;
   }
 
   void packet_delimited() {
@@ -47,23 +55,35 @@ class ProtocolHandler {
 
 public:
   void handleRise() {
-    const unsigned long time = micros();
-    const unsigned long duration = time > last_time ? time - last_time : 0;
-    last_time = time;
+    const unsigned long now = micros();
+    const unsigned long duration = now > last_rise_micros ? now - last_rise_micros : 0;
+    last_rise_micros = now;
 
     if (duration >= MIN_PACKET_SPACING) {
       packet_delimited();
-      delimiter_micros = duration;
+#     if DIAGNOSTICS
+        delimiter_micros = duration;
+#     endif
+      if (reception_stage != BOTCHED_PACKET) {
+        packet_timeout_micros = now + MAX_PACKET_PREAMBLE + MAX_PAYLOAD_TIMEOUT + MAX_PACKET_SPACING;
+      }
       reception_stage = DELIMITED;
     } else if (reception_stage == DELIMITED) {
-      if (duration < MIN_PACKET_PREAMBLE) {
-        abort_packet_train(); // Not a repeat of the previous packet
+      if (duration < MIN_PACKET_PREAMBLE || duration > MAX_PACKET_PREAMBLE) {
+        if (VERBOSE) {
+          Serial.print(duration);
+          Serial.println(" µs rise after delimiter");
+        }
+        cancel_packet();
         return;
       }
-      preamble_micros = duration;
+#     if DIAGNOSTICS
+        preamble_micros = duration;
+#     endif
       reception_stage = OPENED;
       extra_peaks_received = 0;
       bits_received = 0;
+      packet_timeout_micros = now + MAX_PAYLOAD_TIMEOUT + MAX_PACKET_SPACING;
     } else if (reception_stage >= OPENED) {
       if (duration < MIN_PEAK_SPACING) {
         if (VERBOSE) {
@@ -107,22 +127,29 @@ public:
 
   unsigned long receive() {
     noInterrupts();
-    const unsigned long bits = bits_received;
-    const unsigned long bits_time = last_time;
-    const bool has_new = packet_handled != bits
-                        && reception_stage == 32
-                        && extra_peaks_received == int(bits & 1);
+    const unsigned long ss_bits_received = bits_received;
+    const unsigned long ss_last_rise_micros = last_rise_micros;
+    const bool has_new = reception_stage == 32
+                      && extra_peaks_received == int(bits_received & 1)
+                      && bits_received != packet_handled;
     interrupts();
-    if (has_new && micros() > bits_time + MAX_PEAK_SPACING) {
-      packet_handled = bits;
+    const unsigned long now = micros();
+    if (packet_handled && now >= packet_timeout_micros) {
       if (VERBOSE) {
+        Serial.println("Stop expecting packet repeats");
+      }
+      abort_packet_train();
+    }
+    if (has_new && now > ss_last_rise_micros + MAX_PEAK_SPACING) {
+      packet_handled = ss_bits_received;
+#     if DIAGNOSTICS
         Serial.print("After ");
         Serial.print(delimiter_micros);
         Serial.print(" + ");
         Serial.print(preamble_micros);
         Serial.println(" µs:");
-      }
-      return bits;
+#     endif
+      return ss_bits_received;
     } else {
       return 0;
     }
